6916c837860a51effc66f3d813206e9e1e5a2849
warkop619 <navigator619@gmail.com>
2020-02-27 09:38:09 +0700

    a48

---
+++ A48/index.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var data = map[string]string{
+			"Name":    "Moden",
+			"Message": "have a nice day",
+		}
+
+		var t, err = template.ParseFiles("template.html")
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		t.Execute(w, data)
+	})
+
+	fmt.Println("starting web server at http://localhost:8000")
+	http.ListenAndServe(":8000", nil)
+}
